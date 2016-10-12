5736af3920f357839f454cf6019afb9fcfb61046
simplty <simplty@gmail.com>
2016-10-12 14:01:20 +0800

    fisrt commit

---
+++ fileOp/src/main/java/net/simplty/fileOp/App.java
@@ -0,0 +1,13 @@
+package net.simplty.fileOp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
